e7a9b4fbedcd8ad76db634c538daa003f000f550
obianaghag <godwinobianagha@gmail.com>
2025-09-05 15:29:56 +0100

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,10 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+      
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization(allow => [allow.owner()]),
+    
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,12 +21,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+        defaultAuthorizationMode: 'userPool',
     },
-  },
+  
 });
 
 /*== STEP 2 ===============================================================
